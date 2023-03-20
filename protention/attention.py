87c0dbca83c1b08a00e292a5aa4318861e6f99b6
Aksel Lenes <akslenes@gmail.com>
2023-03-20 15:10:44 +0100

    Add get_protT5
    Sentencepiece is needed for the T5 tokenizer

--- protention/attention.py
+++ protention/attention.py
@@ -1,7 +1,9 @@
 from io import StringIO
 from urllib import request
 
+import torch
 from Bio.PDB import PDBParser, Structure
+from transformers import T5EncoderModel, T5Tokenizer
 
 
 def get_structure(pdb_code: str) -> Structure:
@@ -16,6 +18,21 @@ def get_structure(pdb_code: str) -> Structure:
     return structure
 
 
+def get_protT5() -> tuple[T5Tokenizer, T5EncoderModel]:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    tokenizer = T5Tokenizer.from_pretrained(
+        "Rostlab/prot_t5_xl_half_uniref50-enc", do_lower_case=False
+    )
+
+    model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(
+        device
+    )
+
+    model.full() if device == "cpu" else model.half()
+
+    return tokenizer, model
+
+
 def get_attention(
     pdb_code: str, chain_ids: list[str], layer: int, head: int, min_attn: float = 0.2
 ):
@@ -26,6 +43,7 @@ def get_attention(
     structure = get_structure(pdb_code)
 
     # get model
+    tokenizer, model = get_protT5()
 
     # call model
 
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,10 +1,24 @@
 from Bio.PDB.Structure import Structure
+from transformers import T5EncoderModel, T5Tokenizer
 
-from protention.attention import get_structure
+from protention.attention import get_protT5, get_structure
 
 
 def test_get_structure():
     pdb_id = "1AKE"
     structure = get_structure(pdb_id)
+
     assert structure is not None
     assert isinstance(structure, Structure)
+
+
+def test_get_protT5():
+    result = get_protT5()
+
+    assert result is not None
+    assert isinstance(result, tuple)
+
+    tokenizer, model = result
+
+    assert isinstance(tokenizer, T5Tokenizer)
+    assert isinstance(model, T5EncoderModel)
