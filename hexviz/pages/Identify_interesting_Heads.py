18e02d3898c44cea6a6d79231ab20b2b2bbdf44f
Aksel Lenes <akslenes@gmail.com>
2023-03-30 18:25:20 +0200

    Tweak interesting heads view

--- hexviz/pages/Identify_interesting_Heads.py
+++ hexviz/pages/Identify_interesting_Heads.py
@@ -27,12 +27,12 @@ chains = list(structure.get_chains())
 
 sequence = get_sequence(chains[0])
 l = len(sequence)
-slice_start= st.sidebar.number_input(f"Slice start(1-{l})",value=1, min_value=1, max_value=l)
-slice_end = st.sidebar.number_input(f"Slice end(1-{l})",value=50, min_value=1, max_value=l)
+slice_start= st.sidebar.number_input(f"Section start(1-{l})",value=1, min_value=1, max_value=l)
+slice_end = st.sidebar.number_input(f"Section end(1-{l})",value=50, min_value=1, max_value=l)
 truncated_sequence = sequence[slice_start-1:slice_end]
 
 
-st.text(f"Each tile is a heatmap of attention for the slice {slice_start}:{slice_end} of {pdb_id} (chain A) for one attention head.")
+st.markdown(f"Each tile is a heatmap of attention for a section of {pdb_id}(A) from residue {slice_start} to {slice_end}. Adjust the section length and starting point in the sidebar.")
 
 # TODO: Decide if you should get attention for the full sequence or just the truncated sequence
 # Attention values will change depending on what we do.
