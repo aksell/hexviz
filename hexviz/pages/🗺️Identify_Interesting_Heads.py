c2046b3f18fad2ba4e681cf2bbfeaf5a6aa8f114
Aksel Lenes <akslenes@gmail.com>
2023-04-11 11:54:17 +0200

    Default to protein selector expander open

--- hexviz/pages/🗺️Identify_Interesting_Heads.py
+++ hexviz/pages/🗺️Identify_Interesting_Heads.py
@@ -18,7 +18,7 @@ models = [
     Model(name=ModelType.ZymCTRL, layers=36, heads=16),
 ]
 
-with st.expander("Input a PDB id, upload a PDB file or input a sequence"):
+with st.expander("Input a PDB id, upload a PDB file or input a sequence", expanded=True):
     pdb_id = select_pdb()
     uploaded_file = st.file_uploader("2.Upload PDB", type=["pdb"])
     input_sequence = st.text_area("3.Input sequence (Folded with ESMfold) Max 400 resis", "", max_chars=400)
