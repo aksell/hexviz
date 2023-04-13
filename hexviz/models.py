2e9b6ccd1f29ccd4aeaaf7c04875868bf7e014c8
Aksel Lenes <akslenes@gmail.com>
2023-04-13 09:50:51 +0200

    Use gpu device for prot bert

--- hexviz/models.py
+++ hexviz/models.py
@@ -27,19 +27,18 @@ def get_tape_bert() -> tuple[TAPETokenizer, ProteinBertModel]:
     model = ProteinBertModel.from_pretrained('bert-base', output_attentions=True)
     return tokenizer, model
 
+# Streamlit is not able to hash the tokenizer for ZymCTRL
+# With streamlit 1.19 cache_object should work without this
 @st.cache(hash_funcs={Tokenizer: lambda _: None})
 def get_zymctrl() -> tuple[GPT2TokenizerFast, GPT2LMHeadModel]:
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     tokenizer = AutoTokenizer.from_pretrained('nferruz/ZymCTRL')
     model = GPT2LMHeadModel.from_pretrained('nferruz/ZymCTRL').to(device)
-    # here we return things that are on the GPU, streamlit can't cache that I think like it can't cache
-    # the attention weights. Figure out how to do caching of GPU stuff in streamlit better.
-    # maybe object caching + streamlit 1.19?
     return tokenizer, model
 
 @st.cache
 def get_prot_bert() -> tuple[BertTokenizer, BertModel]:
-    # TODO use cuda device here too?
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False )
-    model = BertModel.from_pretrained("Rostlab/prot_bert")
+    model = BertModel.from_pretrained("Rostlab/prot_bert").to(device)
     return tokenizer, model
