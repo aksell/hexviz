3c4fddac9848d3a68b9e4fcc5a1861991eccf62b
Aksel Lenes <akslenes@gmail.com>
2023-04-21 12:52:20 +0200

    Rename subpage to ensure correct ordering in sidebar

--- hexviz/pages/1_🗺️Identify_Interesting_Heads.py
+++ hexviz/pages/1_🗺️Identify_Interesting_Heads.py
@@ -3,11 +3,17 @@ import streamlit as st
 from hexviz.attention import get_attention, get_sequence, get_structure
 from hexviz.models import Model, ModelType
 from hexviz.plot import plot_tiled_heatmap
-from hexviz.view import (menu_items, select_heads_and_layers, select_model,
-                         select_pdb, select_protein, select_sequence_slice)
+from hexviz.view import (
+    menu_items,
+    select_heads_and_layers,
+    select_model,
+    select_pdb,
+    select_protein,
+    select_sequence_slice,
+)
 
 st.set_page_config(layout="wide", menu_items=menu_items)
-st.subheader("Find interesting heads and layers")
+st.title("Identify Interesting Heads")
 
 
 for k, v in st.session_state.items():
@@ -19,10 +25,17 @@ models = [
     Model(name=ModelType.PROT_BERT, layers=30, heads=16),
 ]
 
-with st.expander("Input a PDB id, upload a PDB file or input a sequence", expanded=True):
+with st.expander(
+    "Input a PDB id, upload a PDB file or input a sequence", expanded=True
+):
     pdb_id = select_pdb()
     uploaded_file = st.file_uploader("2.Upload PDB", type=["pdb"])
-    input_sequence = st.text_area("3.Input sequence (Folded with ESMfold) Max 400 resis", "", key="input_sequence", max_chars=400)
+    input_sequence = st.text_area(
+        "3.Input sequence (Folded with ESMfold) Max 400 resis",
+        "",
+        key="input_sequence",
+        max_chars=400,
+    )
     pdb_str, structure, source = select_protein(pdb_id, uploaded_file, input_sequence)
     st.write(f"Using: {source}")
 
@@ -34,9 +47,9 @@ chain_ids = [chain.id for chain in chains]
 if "selected_chain" not in st.session_state:
     st.session_state.selected_chain = chain_ids[0]
 chain_selection = st.sidebar.selectbox(
-    label = "Select Chain",
-    options = chain_ids,
-    key = "selected_chain",
+    label="Select Chain",
+    options=chain_ids,
+    key="selected_chain",
 )
 
 selected_chain = next(chain for chain in chains if chain.id == chain_selection)
@@ -44,16 +57,20 @@ sequence = get_sequence(selected_chain)
 
 l = len(sequence)
 slice_start, slice_end = select_sequence_slice(l)
-truncated_sequence = sequence[slice_start-1:slice_end]
+truncated_sequence = sequence[slice_start - 1 : slice_end]
 
 
 layer_sequence, head_sequence = select_heads_and_layers(st.sidebar, selected_model)
 
-st.markdown(f"Each tile is a heatmap of attention for a section of the {source} chain ({chain_selection}) from residue {slice_start} to {slice_end}. Adjust the section length and starting point in the sidebar.")
+st.markdown(
+    f"Each tile is a heatmap of attention for a section of the {source} chain ({chain_selection}) from residue {slice_start} to {slice_end}. Adjust the section length and starting point in the sidebar."
+)
 
 # TODO: Decide if you should get attention for the full sequence or just the truncated sequence
 # Attention values will change depending on what we do.
 attention = get_attention(sequence=truncated_sequence, model_type=selected_model.name)
 
-fig = plot_tiled_heatmap(attention, layer_sequence=layer_sequence, head_sequence=head_sequence)
+fig = plot_tiled_heatmap(
+    attention, layer_sequence=layer_sequence, head_sequence=head_sequence
+)
 st.pyplot(fig)
